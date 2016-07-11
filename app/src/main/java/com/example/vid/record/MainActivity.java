30d34eb4547fb772ad71536656119a69c4388141
Vid Kumse <vid.kumse@gmail.com>
2016-07-11 20:03:53 +0200

    Test Volley knjižnice. Ob pritisku na gumb pošlje request na testni server

--- app/src/main/java/com/example/vid/record/MainActivity.java
+++ app/src/main/java/com/example/vid/record/MainActivity.java
@@ -8,11 +8,21 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText mass;
     String massValue;
     int[] massArray={89,88,87};
+    TextView test;
 
 
     @Override
@@ -22,17 +32,38 @@ public class MainActivity extends AppCompatActivity {
 
         mass = (EditText) findViewById(R.id.vnos_teze);
 
-        String patientIdEhr = "50bc1c98-8540-4d86-bbc7-813f7c55377e";
-
-        
     }
 
     public void sendMessage(View v) {
 
         massValue = mass.getText().toString();
 
-        Intent i = new Intent(MainActivity.this, Form.class);
+        test = (TextView) findViewById(R.id.test);
+
+        String patientIdEhr = "50bc1c98-8540-4d86-bbc7-813f7c55377e";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = "http://ip.jsontest.com/";
+
+        //request
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.POST, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        test.setText(response.toString());
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        test.setText("Prišlo je do napake med identifikacijo na strežniku!");
+                    }
+                });
+
+        queue.add(jsObjRequest);
+
+        /*Intent i = new Intent(MainActivity.this, Form.class);
         i.putExtra("mass", massArray);
-        startActivity(i);
+        startActivity(i);*/
     }
 }
