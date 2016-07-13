199812ec991a0bfc2bb7beca6686b5ae8c2b509e
Vid Kumse <vid.kumse@gmail.com>
2016-07-13 11:42:05 +0200

    UI prva verzija implementirana

--- app/src/main/java/com/example/vid/record/Form.java
+++ app/src/main/java/com/example/vid/record/Form.java
@@ -3,6 +3,8 @@ package com.example.vid.record;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
@@ -25,6 +27,18 @@ public class Form extends AppCompatActivity {
     String sessionId="";
     String mass2;
 
+    EditText body_weight_form;
+    EditText body_fluids_form;
+    EditText heart_rate_form;
+    EditText circ_form;
+
+    String body_weight;
+    String body_fluids;
+    String heart_rate;
+    String circ;
+    String position_hr;
+    String position_circ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +50,11 @@ public class Form extends AppCompatActivity {
         test = (TextView) findViewById(R.id.test);
 
         mass2 = Integer.toString(massArray[2]);
+
+        body_weight_form = (EditText) findViewById(R.id.weight_form);
+        body_fluids_form = (EditText) findViewById(R.id.body_fluid_form);
+        heart_rate_form = (EditText) findViewById(R.id.heart_rate_form);
+        circ_form = (EditText) findViewById(R.id.circumference_form);
     }
 
     public void sendMessage(View v) {
@@ -57,6 +76,12 @@ public class Form extends AppCompatActivity {
         int day = c.get(Calendar.DAY_OF_MONTH);
         int month = c.get(Calendar.MONTH);
         int year = c.get(Calendar.YEAR);
+
+        //Pridobivanje podatkov
+        body_fluids = body_fluids_form.getText().toString();
+        body_weight = body_weight_form.getText().toString();
+        circ = circ_form.getText().toString();
+        heart_rate = heart_rate_form.getText().toString();
 
         //Sestavimo format časa
         final String time = year+"-"+month+"-"+day+"T"+hours+":"+minutes+"Z";
@@ -87,16 +112,16 @@ public class Form extends AppCompatActivity {
         Map<String,String> params = new HashMap<String, String>();
         params.put("ctx/language","en");
         params.put("ctx/territory", "SI");
-        params.put("encounter/body_weight:0/any_event:0/weight|magnitude", "70");
+        params.put("encounter/body_weight:0/any_event:0/weight|magnitude", body_weight);
         params.put("encounter/body_weight:0/any_event:0/weight|unit", "kg");
-        params.put("encounter/pulse_heart_beat:0/any_event:0/position", "at1003");
-        params.put("encounter/body_water:0/total_water_percentage|magnitude", "92");
+        params.put("encounter/pulse_heart_beat:0/any_event:0/position", position_hr);
+        params.put("encounter/body_water:0/total_water_percentage|magnitude", body_fluids);
         params.put("encounter/body_water:0/total_water_percentage|unit", "%");
-        params.put("encounter/pulse_heart_beat:0/any_event:0/heart_rate|magnitude", "69");//
+        params.put("encounter/pulse_heart_beat:0/any_event:0/heart_rate|magnitude", heart_rate);//
         params.put("encounter/pulse_heart_beat:0/any_event:0/heart_rate|unit", "/min");
-        params.put("encounter/measurement_of_body_segment:0/ankle_circumference:0|magnitude", "23");
+        params.put("encounter/measurement_of_body_segment:0/ankle_circumference:0|magnitude", circ);
         params.put("encounter/measurement_of_body_segment:0/ankle_circumference:0|unit", "cm");
-        params.put("encounter/measurement_of_body_segment:0/position", "at0037");
+        params.put("encounter/measurement_of_body_segment:0/position", position_circ);
 
         //DRUGI REQUEST. V glavi pošljemo sessionID, v body-ju pa podatke za vpis. Oboje je v formatu JSON
         JsonObjectRequest jsObjRequest2 = new JsonObjectRequest
@@ -122,4 +147,33 @@ public class Form extends AppCompatActivity {
         };
         queue.add(jsObjRequest2);
     };
+
+    public void onRadioButtonClicked(View view) {
+        //Preverimo, če je gumb pritisnjen
+        boolean checked = ((RadioButton) view).isChecked();
+
+        //Preverimo, kateri gumb je bil  pritisnjen
+        switch(view.getId()) {
+            case R.id.standing_radio:
+                if (checked)
+                    position_circ = "at0037";
+                    position_hr = "at1003";
+                break;
+            case R.id.Reclining_radio:
+                if (checked)
+                    position_circ="at0038";
+                    position_hr="at1001";
+                break;
+            case R.id.Sitting_radio:
+                if (checked)
+                    position_circ = "at0039";
+                    position_hr = "at0039";
+                break;
+            case R.id.Lying_radio:
+                if (checked)
+                    position_circ = "at0040";
+                    position_hr = "at1000";
+                break;
+        }
+    }
 }
