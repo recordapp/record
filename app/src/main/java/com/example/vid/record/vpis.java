c2f52004684fe84ea8f02c4b210ac50ae4282ee9
Vid Kumse <vid.kumse@gmail.com>
2016-07-15 01:58:05 +0200

    vpis activity dodan + izgled

---
+++ app/src/main/java/com/example/vid/record/vpis.java
@@ -0,0 +1,13 @@
+package com.example.vid.record;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class vpis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vpis);
+    }
+}
