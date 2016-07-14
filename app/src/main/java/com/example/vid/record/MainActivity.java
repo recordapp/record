3f7b51a6046ac67d8e3f93983fb0e6d36826d7a2
Vid Kumse <vid.kumse@gmail.com>
2016-07-14 18:24:13 +0200

    Parsanje JSON, potrebno je še narediti zanko

--- app/src/main/java/com/example/vid/record/MainActivity.java
+++ app/src/main/java/com/example/vid/record/MainActivity.java
@@ -14,6 +14,7 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.github.scribejava.apis.TwitterApi;
@@ -24,9 +25,12 @@ import com.github.scribejava.core.model.OAuthRequest;
 import com.github.scribejava.core.model.Verb;
 import com.github.scribejava.core.oauth.OAuth10aService;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
@@ -66,7 +70,8 @@ public class MainActivity extends AppCompatActivity {
 
         String url = "https://rest.ehrscape.com/rest/v1/session?username=medrockweek1&password=medrockweek1";
 
-        String url2 ="https://rest.ehrscape.com/rest/v1/query/?aql=select%20%20%20%20%20a_a/data%5Bat0002%5D/events%5Bat0003%5D/data%5Bat0001%5D/items%5Bat0004%5D/value/magnitude%20as%20Weight_magnitude,%20%20%20%20%20a_b/data%5Bat0001%5D/events%5Bat0002%5D/data%5Bat0003%5D/items%5Bat0004%5D/value/magnitude%20as%20Total_water_percentage_magnitude,%20%20%20%20%20a_a/data%5Bat0002%5D/origin/value%20from%20EHR%20e%20contains%20COMPOSITION%20a%20contains%20(%20%20%20%20%20OBSERVATION%20a_a%5BopenEHR-EHR-OBSERVATION.body_weight.v1%5D%20and%20%20%20%20%20OBSERVATION%20a_b%5BopenEHR-EHR-OBSERVATION.body_water.v0%5D)%20offset%200%20limit%20100";
+        String query = "select%20%20%20%20%20a_a/data%5Bat0002%5D/events%5Bat0003%5D/data%5Bat0001%5D/items%5Bat0004%5D/value%20as%20Weight,%20%20%20%20%20a/context/start_time,%20%20%20%20%20e/ehr_id/value,%20%20%20%20%20a_b/data%5Bat0001%5D/events%5Bat0002%5D/data%5Bat0003%5D/items%5Bat0004%5D/value%20as%20Total_water_percentage%20from%20EHR%20e%20contains%20COMPOSITION%20a%20contains%20(%20%20%20%20%20OBSERVATION%20a_a%5BopenEHR-EHR-OBSERVATION.body_weight.v1%5D%20and%20%20%20%20%20OBSERVATION%20a_b%5BopenEHR-EHR-OBSERVATION.body_water.v0%5D)%20where%20%20%20%20%20e/ehr_id/value%3D'50bc1c98-8540-4d86-bbc7-813f7c55377e'%20and%20%20%20%20%20a/context/start_time%3E'2016-07-13T09:57:26.346%2B02:00'%20offset%200%20limit%20100";
+        String url2 ="https://rest.ehrscape.com/rest/v1/query/?aql="+query;
 
         //request
         //PRVI REQUEST. V URL-ju pošljemo geslo in username, kot odgovor pa dobimo SessionID.
@@ -98,7 +103,27 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         test.setText("Vpis je bil uspešno poslan!");
-                        test.setText(response.getString(""));
+                        try {
+
+                            String resultSet = response.getString("resultSet");
+                            JSONArray resultSetJSON = new JSONArray(resultSet);
+                            JSONObject weightJSON = resultSetJSON.getJSONObject(3);
+                            test.setText("ss");
+                            String weight = weightJSON.getString("Weight");
+                            JSONObject weight1JSON = new JSONObject(weight);
+                            String weight1 = weight1JSON.getString("units");
+                            test.setText(weight1.toString());
+
+                            /*for(int i = 0; i<resultSetJSON.length(); i++) {
+                                JSONObject meritevJSON = resultSetJSON.getJSONObject(i);
+                                String meritev = meritevJSON.getString();
+                                JSONObject tezaJSON = meritevJSON.getJSONObject();
+                                ArrayList<JSONObject> seznam_meritev = resultSetJSON.getJSONObject(i);
+                            }*/
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            test.setText("Napaka pri parsanju");
+                        }
                     }
                 }, new Response.ErrorListener() {
                     @Override
