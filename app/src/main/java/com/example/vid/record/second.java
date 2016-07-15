1e1d929a7af90b4606c974b69ef6a003a5d0768f
Vid Kumse <vid.kumse@gmail.com>
2016-07-15 02:26:13 +0200

    Dodani vsi 4 xml-ji, manjka povezava z javo

--- app/src/main/java/com/example/vid/record/second.java
+++ app/src/main/java/com/example/vid/record/second.java
@@ -1,7 +1,9 @@
 package com.example.vid.record;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class second extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class second extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void record2(View v) {
+        Intent i = new Intent(second.this, results.class);
+        startActivity(i);
     }
 }
