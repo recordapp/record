6f21eb31a296d7fac4bfde288155b562564def89
Vid Kumse <vid.kumse@gmail.com>
2016-07-15 02:07:44 +0200

    Dodan drugi xml... potrebno ga je še povezati z javo

--- app/src/main/java/com/example/vid/record/MainActivity.java
+++ app/src/main/java/com/example/vid/record/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mass = (EditText) findViewById(R.id.vnos_teze);
+        //mass = (EditText) findViewById(R.id.vnos_teze);
 
         test = (TextView) findViewById(R.id.test);
 
--- app/src/main/java/com/example/vid/record/vpis.java
+++ app/src/main/java/com/example/vid/record/vpis.java
@@ -1,7 +1,9 @@
 package com.example.vid.record;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class vpis extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class vpis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vpis);
+    }
+
+    public void sign(View v) {
+        Intent i = new Intent(vpis.this, MainActivity.class);
+        startActivity(i);
     }
 }
